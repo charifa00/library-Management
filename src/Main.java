40024741d18a95a522c4acdfa72ef61059dca828
IFA-0_0 <charifadr432@gmail.com>
2024-10-14 10:56:26 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import library.*;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Library library = new Library();
+// Example of user input for adding a book
+        System.out.print("Enter book title: ");
+        String title = scanner.nextLine();
+        System.out.print("Enter book author: ");
+        String author = scanner.nextLine();
+        System.out.print("Enter book isbn: ");
+        String isbn= scanner.nextLine();
+
+
+
+    }
+}
